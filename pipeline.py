df0bd3c484aa497b3d168728f6c8d89e5dcd612e
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-10-16 16:15:50 +0100

    entrainement des models

--- pipeline.py
+++ pipeline.py
@@ -12,7 +12,6 @@ def charge_donnes():
     data =  pd.read_csv('data.csv')
     return data
 data = charge_donnes()
-# print(data)
 
 def pretraitement_donnes(data):
 
@@ -23,20 +22,18 @@ def pretraitement_donnes(data):
     for column in data:
         if data[column].dtypes == 'object':
             data[column] = data[column].fillna(data[column].mode()[0])
-
-
-
 # 2- Supprimer les colonnes inutiles:
     data = data.drop(columns=['Order_ID', 'Courier_Experience_yrs', 'Vehicle_Type', 'Time_of_Day'])
+    clean_data=data
+    return clean_data
 
-# 3- Encoder les variables categprielles:
-    ca_columns = ['Weather', 'Traffic_Level']
-    encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
-    encoder_columns = encoder.fit_transform(data[ca_columns])
-    encoder_f=pd.DataFrame(encoder_columns,columns=encoder.get_feature_names_out(ca_columns))
-    data = pd.concat([data.drop(columns= ca_columns),encoder_f],axis=1)
-    return data
-clean_data = pretraitement_donnes(data)
+# # 3- Encoder les variables categprielles:
+#     ca_columns = ['Weather', 'Traffic_Level']
+#     encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+#     encoder_columns = encoder.fit_transform(data[ca_columns])
+#     encoder_f=pd.DataFrame(encoder_columns,columns=encoder.get_feature_names_out(ca_columns))
+#     data = pd.concat([data.drop(columns= ca_columns),encoder_f],axis=1)
+#     return data
 
 
 # # 1- concat clean_data et num_columns:
@@ -50,14 +47,12 @@ def split_features(data):
     x = data.drop('Delivery_Time_min', axis=1)
     y = data['Delivery_Time_min']
     return x,y
-x,y = split_features(clean_data)
 
 
 def split_data(x,y):
 # 1- train_test_split:
     x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2)
     return x_train,x_test,y_train,y_test
-x_train,x_test,y_train,y_test = split_data(x,y)
 
 
 def normalized_data(x_train,x_test):
@@ -66,7 +61,6 @@ def normalized_data(x_train,x_test):
     x_train_scaler = scaler.fit_transform(x_train)
     x_test_scaler = scaler.transform(x_test)
     return x_train_scaler,x_test_scaler
-x_train_scaled, x_test_scaled = normalized_data(x_train, x_test)
 
 
 def selectKBest_features(x_train,y_train):
@@ -76,9 +70,10 @@ def selectKBest_features(x_train,y_train):
     selected_features = np.array(x_train.columns)[selector.get_support()]
     return x_new
 
-x_train,x_test,y_train,y_test = split_data(x,y)
+# x_train,x_test,y_train,y_test = split_data(x,y)
 
-x_new = selectKBest_features(x_train, y_train)
+# x_new = selectKBest_features(x_train, y_train)
+
 
 
 
