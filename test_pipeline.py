060de2637a6458a102dfc347a4b0f3b9c750cd40
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-10-17 13:08:51 +0100

    test du mae

--- test_pipeline.py
+++ test_pipeline.py
@@ -2,20 +2,59 @@ import pandas as pd
 import numpy as np
 import pytest
 from pipeline import pretraitement_donnes, split_features, split_data
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+
 
 @pytest.fixture
 def charge_data():
     data = pd.read_csv('data.csv')
     return data
 
-def test(charge_data):
+# 1- Verification des dimensions des données :
+def test_len(charge_data):
     data = charge_data
-    data_clean =pretraitement_donnes(charge_data)
+    data_clean =pretraitement_donnes(data)
     x,y = split_features(data_clean)
     x_test,x_train,y_test,y_train = split_data(x,y)
    
     assert len(x_test) == len(y_test)
     assert len(x_train) == len(y_train)
     
+
+
+# 2- Vérification que la MAE maximale ne dépasse pas un seuil défini(RandomForestRegressor) :
+def mae_max_RandomForestRegressor():
+    data = charge_data
+    data_clean = pretraitement_donnes(data)
+    x,y = split_features(data_clean)
+    x_test,y_test,x_train,y_train = split_data(x,y)
+
+    model = RandomForestRegressor(random_state=42)
+    model.fit(x_train,y_train)
+    y_pred_model = model.predict(x_test)
+
+    mae_model = mean_absolute_error(y_test,y_pred_model)
+    assert mae_model < 7 
+
+
+
+
+
+# 3- Vérification que la MAE maximale ne dépasse pas un seuil défini(SVR) :
+def mae_max_SVR():
+    data = charge_data
+    data_clean = pretraitement_donnes(data)
+    x,y = split_features(data_clean)
+    x_test,y_test,x_train,y_train = split_data(x,y)
+
+    svr = SVR()
+    svr.fit(x_train,y_train)
+    y_pred_svr = svr.predict(x_test)
+
+
+    mae_svr = mean_absolute_error(y_test,y_pred_svr)
+    assert mae_svr < 7 
 
 
